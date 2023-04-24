2953c45429c03ed096754dc36b6b59c19963ef0b
lik3as <lik3games@gmail.com>
2023-04-24 17:15:01 -0300

    Project Init

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+module.exports = {
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: 'postgres',
+  }
+};
