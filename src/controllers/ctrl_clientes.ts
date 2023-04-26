cbb00809be3711f16abca9c3770fec818812b49b
lik3as <daviacosta2006@gmail.com>
2023-04-26 17:25:04 -0300

    ADD Controllers

---
+++ src/controllers/ctrl_clientes.ts
@@ -0,0 +1,13 @@
+import {Cliente, body_cliente} from '../models/index'
+
+type body = Promise<body_cliente[] | undefined>
+class ClienteCtrl{
+  private _all: body;
+
+  constructor(){ }
+
+  get all(): body{
+    this._all = Cliente.findAll();
+    return this.all;
+  }
+}
