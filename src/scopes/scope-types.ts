a4c982575721d13f5fb4277eb318c22f915549a7
lik3as <daviacosta2006@gmail.com>
2023-04-28 17:16:46 -0300

    MODIFIED method and model types

--- src/scopes/scope-types.ts
+++ src/scopes/scope-types.ts
@@ -9,16 +9,24 @@ export declare type ScopesOptionsGetter = () => {
 
 export declare function Scopes(scopesGetter: ScopesOptionsGetter): Function;
 
-export type rel_prod = 'prod' | 'merc';
-export type rel_prodFab = 'fab' | 'prod';
-export type rel_prod_categories = 'tipo' | 'subtipo' | 'marca' | 'modelo';
-export type rel_fab = 'prod';
-export type rel_merc = 'prod' | 'kit';
-export type rel_logi = ['merc', any]
-export type rel_kit = 'prod'
-export type rel_cliente = ['venda', any]
 
-export type methods = 'join_in';
+export type rel_prod = ['prod' | 'merc'];
+export type rel_prodFab = ['fab' | 'prod'];
+export type rel_prod_categories = ['tipo' | 'subtipo' | 'marca' | 'modelo', string | undefined];
+export type rel_fab = ['prod', number | undefined];
+export type rel_merc = 'prod' | 'kit';
+export type rel_logi = 'merc'
+export type rel_kit = 'prod'
+export type rel_cliente = 'venda'
+
+/**
+ * @type {method} - É a primeira parte do nome do escopo a ser utilizado
+ * @example - Por exemplo: 'join_in_' + 'prod' resulta no na funcão de escopo * join_in_prod(parâmetros), seus parâmetros são
+ * exigidos na classe controller.
+ * 
+ */
+
+export type method = 'join_in_' | 'find_by_id';
 
 //The specific type for the inner join scope
 export type join = IncludeOptions | undefined | Includeable[] | Includeable | ModelOptions<any> 
