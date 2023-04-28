b9ee703728636acf2382d893d89715f684764bab
lik3as <daviacosta2006@gmail.com>
2023-04-28 10:58:29 -0300

    MODIFIED scopes types logic

--- src/scopes/scope-types.ts
+++ src/scopes/scope-types.ts
@@ -7,21 +7,18 @@ export declare type ScopesOptionsGetter = () => {
     [sopeName: string]: ScopesOptions;
 };
 
-
 export declare function Scopes(scopesGetter: ScopesOptionsGetter): Function;
 
-//The type of the functions names
-export type prod = ['join_in_prod' | 'join_in_tipo' | 'join_in_subtipo' | 'join_in_marca'
-| 'join_in_modelo' | 'join_in_merc' | 'join_in_categories', boolean | string]
-export type prodKit = ['join_in_prod' | 'join_in_kit', any];
-export type prodFab = ['join_in_fab' | 'join_in_prod', any]
-export type modelo = ['join_in_prod', any]
-export type fab = ['join_in_prod', any];
-export type merc = ['join_in_prod' | 'join_in_kit', any];
-export type marca = ['join_in_prod', any];
-export type logi = ['join_in_merc', any]
-export type kit = 'join_in_prod'
-export type cliente = ['join_in_venda', any]
+export type rel_prod = 'prod' | 'merc';
+export type rel_prodFab = 'fab' | 'prod';
+export type rel_prod_categories = 'tipo' | 'subtipo' | 'marca' | 'modelo';
+export type rel_fab = 'prod';
+export type rel_merc = 'prod' | 'kit';
+export type rel_logi = ['merc', any]
+export type rel_kit = 'prod'
+export type rel_cliente = ['venda', any]
+
+export type methods = 'join_in';
 
 //The specific type for the inner join scope
 export type join = IncludeOptions | undefined | Includeable[] | Includeable | ModelOptions<any> 
