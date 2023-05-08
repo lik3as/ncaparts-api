5e5d35252903e221dd660453f856aac7409617f8
lik3as <daviacosta2006@gmail.com>
2023-05-08 14:40:18 -0300

    CHANGED File names AND Added a method to get all tuples

---
+++ src/services/srv.cli.ts
@@ -0,0 +1,39 @@
+import sequelize, { Cliente } from "../models/index";
+import IFab, {param_body, param_bodies, body} from '../contracts/IServices'
+
+
+export default class ClienteCtrl implements IFab<Cliente>{
+  constructor(){}
+
+  public async getBodies({method, on, args}: param_bodies) : body<Cliente[]> { 
+    return (typeof args == undefined ) ?
+     Cliente.scope(
+      {method: `${method}${on}`}
+      ).findAll()
+      :
+      Cliente.scope(
+      {method: [`${method}${on}`, args]}
+      ).findAll()
+  }
+
+  public async getBody({ method, on, args }: param_body): body<Cliente> {
+    if (method!='find_by_')
+      throw new Error("Este método retorna uma lista.");
+    return Cliente.scope(
+      {method: [`${method}${on}`, args]}
+      ).findOne()
+  }
+
+  public async getAllBodies(): Promise<Cliente[]> {
+    return Cliente.findAll({
+      order: [
+        sequelize.fn('concat', sequelize.col('nome'))
+      ]
+    })
+  }
+
+  public async create(body: {}): Promise<void> {
+    await Cliente.create(body)
+  }
+
+}
---
+++ src/services/srv.ven.ts
@@ -0,0 +1,28 @@
+import { Venda } from "../models/index";
+import IFab, {param_body, param_bodies, body} from '../contracts/IServices'
+
+
+export default class VendaCtrl implements IFab<Venda>{
+  constructor(){ }
+
+  public async getBodies({method, on, args}: param_bodies) : body<Venda[]> { 
+    return (typeof args == undefined ) ?
+     Venda.scope(
+      {method: `${method}${on}`}
+      ).findAll()
+      :
+      Venda.scope(
+      {method: [`${method}${on}`, args]}
+      ).findAll()
+  }
+
+  public async getBody({ method, on, args }: param_body): body<Venda> {
+    if (method!='find_by_')
+      throw new Error("Este método retorna uma lista.");
+    return Venda.scope(
+      {method: [`${method}${on}`, args]}
+      ).findOne()
+  }
+}
+
+const fabri: VendaCtrl = new VendaCtrl()
