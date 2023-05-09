79e369dba9bce79e501acacc20e0d5a344c89eb1
lik3as <lik3games@gmail.com>
2023-05-09 16:54:42 -0300

    changed Client to Cli at index

---
+++ controllers/ctrl.prod.ts
@@ -0,0 +1,38 @@
+import ProdutoCtrl from "../services/srv.prod";
+import { Request, Response, NextFunction } from "express";
+
+const ctrl = new ProdutoCtrl();
+
+const on_error = (err: any) => {
+  console.log('An error occured while trying to access Products route: \n' + 
+  `\x1b[31m${err}\x1b[0m`)
+}
+
+export default {
+
+  /**
+   * @returns Fifty latest products ordered by name 
+   */
+  async latest(req: Request, res: Response) {
+    return res.json(await ctrl.getAllBodies().catch(on_error))
+  },
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    if (req.get('categoria') != undefined) {
+      next('route')
+    }
+
+    res.json({'placeholder': 'placeholder'})
+  },
+
+  async create_categoria(req: Request, res: Response, next: NextFunction) {
+    await ctrl.createCategoria(req.get('categoria'), req.body).catch(on_error)
+    
+    res.json({'placeholder': 'placeholder'})
+  },
+
+  async get_categorias(req: Request, res: Response) {
+    return res.json(await ctrl.getCategorias('Tipo').catch(on_error));
+
+  }
+}
