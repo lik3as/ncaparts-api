3232a291b5501e8dbf0f16c70fb2e17fe3170efb
lik3as <daviacosta2006@gmail.com>
2023-05-09 16:17:17 -0300

    index now exporting as a package

--- src/routes/router.cli.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import ctrl_cli from '../controllers/ctrl.cli';
-
-const router = Router()
-
-router.post('/cli', ctrl_cli.create);
-router.get('/cli', ctrl_cli.all);
-
-export default router;
