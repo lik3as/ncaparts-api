bb5b0910a921932735e25927968ad4a0383248a5
lik3as <daviacosta2006@gmail.com>
2023-05-09 16:24:40 -0300

    middleware isn't part of the package

--- src/middleware/info.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export default ((req: Request, res: Response, next: NextFunction) => {
-  let date = new Date().toLocaleString()
-  console.log(`\nA client (\x1b[35m${req.ip}\x1b[0m) made a \x1b[35m${req.method} \x1b[0m Requistion at: \x1b[35m${date}\x1b[0m`+ 
-  `\n${req.url} route`)
-  next()
-})
