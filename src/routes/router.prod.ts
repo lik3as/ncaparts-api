f83362d0494e323cbd99b555534eed000edad940
lik3as <lik3games@gmail.com>
2023-05-09 09:31:25 -0300

    ADD Rotues for controllers Prod and Cli

--- src/routes/router.prod.ts
+++ src/routes/router.prod.ts
@@ -4,5 +4,7 @@ import ctrlProd from '../controllers/ctrl.prod';
 const router = Router()
 
 router.get('/prod', ctrlProd.latest);
+router.get('/prod/cat', ctrlProd.get_categorias);
+router.post('/prod/cat', ctrlProd.create_categoria);
 
 export default router;
