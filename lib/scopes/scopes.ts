dd917e5d154eb391af8b4fbf12c0a33f00711457
lik3as <lik3games@gmail.com>
2023-05-10 13:48:50 -0300

    ncaparts-rest pseudo-initial commit

--- lib/scopes/scopes.ts
+++
@@ -1,7 +0,0 @@
-/*
-*   Dentro dos scopes será aplicada até uma subquery por
-*   scope. Caso seja necessário fazer uma pesquisa com mais
-*   de um subquery, apenas utilize os finders.
-*/
-export {prod_scopes} from './scope_prod'
-export {fab_scopes} from './scope_fab'
