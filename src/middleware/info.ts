ebf7695d643f3cfeba8b1e894d082ea1130f271f
lik3as <lik3games@gmail.com>
2023-05-11 08:36:04 -0300

    removed extra files

--- src/middleware/info.ts
+++ src/middleware/info.ts
@@ -5,4 +5,4 @@ export default ((req: Request, res: Response, next: NextFunction) => {
   console.log(`\nA client (\x1b[35m${req.ip}\x1b[0m) made a \x1b[35m${req.method} \x1b[0m Requistion at: \x1b[35m${date}\x1b[0m`+ 
   `\n${req.url} route`)
   next()
-})
+});
