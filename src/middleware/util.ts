4898236b6d8a75c0c9f726bb5fe27c2bc9071aab
lik3as <daviacosta2006@gmail.com>
2023-05-18 17:29:09 -0300

    middleware request info

---
+++ src/middleware/util.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+import { sequelize } from 'ncaparts-db'
+
+export default {
+  info(req: Request, res: Response, next: NextFunction) {
+    let date = new Date().toLocaleString()
+    console.log(`\nA client (\x1b[35m${req.ip}\x1b[0m) made a \x1b[35m${req.method} \x1b[0m Requistion at: \x1b[35m${date}\x1b[0m`+ 
+    `\n${req.url} route`)
+
+    next()
+  },
+};
