6724f39e1d5f68bbe47120d5628c9c53ad933e9d
lik3as <daviacosta2006@gmail.com>
2023-05-19 14:24:45 -0300

    manager prod

--- src/routes/tables/router.prod.ts
+++ src/routes/tables/router.prod.ts
@@ -4,8 +4,10 @@ import ctrlProd from '../../controllers/ctrl.prod';
 const router = Router()
 
 router.post('/tables/Produtos', ctrlProd.create);
+router.post('/tables/Produtos/:cat', ctrlProd.create_categoria);
 router.get('/tables/Produtos', ctrlProd.latest);
 router.get('/tables/Produtos/columns', ctrlProd.get_columns);
 router.get('/tables/Produtos/:cat', ctrlProd.get_categorias);
+router.get('/tables/Produtos/:cat/columns', ctrlProd.get_cat_columns);
 
 export default router;
