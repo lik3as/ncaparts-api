44325c07ac7a8566584c28ecaf5e03db70de7166
lik3as <lik3games@gmail.com>
2023-06-26 16:53:10 -0300

    routes for mercname and prod updates

--- src/routes/tables/router.prod.ts
+++ src/routes/tables/router.prod.ts
@@ -3,9 +3,10 @@ import ctrlProd from '../../controllers/ctrl.prod';
 
 const router = Router()
 
-router.post('/tables/Produtos', ctrlProd.create);
+router.post('/tables/Produtos', ctrlProd.create_many, ctrlProd.update);
 router.post('/tables/Produtos/:cat', ctrlProd.create_categoria);
-router.get('/tables/Produtos', ctrlProd.latest);
+router.get('/tables/Produtos/records', ctrlProd.count);
+router.get('/tables/Produtos', ctrlProd.latest, ctrlProd.getOne);
 router.get('/tables/Produtos/columns', ctrlProd.get_columns);
 router.get('/tables/Produtos/:cat', ctrlProd.get_categorias);
 router.get('/tables/Produtos/:cat/columns', ctrlProd.get_cat_columns);
