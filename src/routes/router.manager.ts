70574337e3d374a55400ff181e271f31f4a764c8
lik3as <daviacosta2006@gmail.com>
2023-06-26 16:52:30 -0300

    manager including more routes

--- src/routes/router.manager.ts
+++ src/routes/router.manager.ts
@@ -3,6 +3,7 @@ import ctrl_tables from '../controllers/ctrl.manager';
 
 const router: Router = Router()
 
+router.get('/:table/Records', ctrl_tables.redirect);
 router.get('/tables', ctrl_tables.get_tables);
 router.get('/:table/columns', ctrl_tables.get_table_columns);
 router.get('/:table', ctrl_tables.get_table);
