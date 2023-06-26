04fd6971c1ed290dac9065ec35e56f02149cb887
lik3as <lik3games@gmail.com>
2023-06-26 16:53:31 -0300

    add types and implemented routes in index

---
+++ src/types/IMercadoria.ts
@@ -0,0 +1,10 @@
+export default interface IMercadoria{
+  produto: string,
+  skus_relacionados: string,
+  kit: string | null,
+  importada: boolean,
+  disponivel: boolean,
+  valor_real: number,
+  valor_real_revenda: number | null,
+  nome: string
+}
