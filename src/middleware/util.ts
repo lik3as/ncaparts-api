97e716d554e44fac80e45cb3fef6a746b87b7103
lik3as <daviacosta2006@gmail.com>
2023-06-26 16:51:47 -0300

    finished util

--- src/middleware/util.ts
+++ src/middleware/util.ts
@@ -9,4 +9,10 @@ export default {
 
     next()
   },
+
+  allow_origin(req: Request, res: Response, next: NextFunction) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  }
 };
