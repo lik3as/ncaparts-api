8b04ca238ee27a283dcb7ef29d4b16ed51895438
lik3as <daviacosta2006@gmail.com>
2023-06-26 16:50:15 -0300

    finished prod

--- src/controllers/ctrl.prod.ts
+++ src/controllers/ctrl.prod.ts
@@ -1,9 +1,10 @@
-import { sProd, sequelize } from 'ncaparts-db'
+import { sProd } from 'ncaparts-db'
 import { Request, Response, NextFunction } from "express";
 
 const ctrl = new sProd();
-const _prod  = new sProd.skeleton();
-type prod = typeof _prod | void;
+const skeleton  = new sProd.skeleton();
+
+type Produto = typeof skeleton ;
 
 const on_error = (err: any) => {
   console.log('An error occured while trying to access Products route: \n' + 
@@ -22,14 +23,36 @@ export default {
   /**
    * @returns Fifty latest products ordered by name 
    */
-  async latest(req: Request, res: Response) {
-    return res.json(await ctrl.getAllBodies().catch(on_error))
+  async latest(req: Request, res: Response, next: NextFunction) {
+    if (typeof req.query.sku !== 'undefined' && req.query.sku != '') return next();
+    if (typeof req.query.page === 'undefined') req.query.page = '0'
+
+    const page: number = +(req.query.page);
+    return res.json(await ctrl.getAllBodies(page));
   },
 
-  async create(req: Request, res: Response, next: NextFunction) {
+  async count(req: Request, res: Response, next: NextFunction) {
+    const count = await ctrl.records();
+    return res.json(count);
+  },
+
+  async getOne(req: Request, res: Response) {
+    const sku: string = req.query.sku as string;
+
+    const prod = (await ctrl.getBody({method: 'find_by_',
+     on: 'unique', args: sku}))!;
+
+    res.json(prod);
+  },
+
+  async create_many(req: Request, res: Response, next: NextFunction) {
+    const query = req.query;
+    if (query.a == "update") {
+      return next();
+    }
 
     let bodies: Object[] = req.body;
-    let created: prod[] = [];
+    let created: Produto[] = [];
 
     //De nome para id, pronto para a inserção
     for (let i: number = 0 ; i < bodies.length; i++){
@@ -60,12 +83,51 @@ export default {
 
       if (body != null) {
         const creation = await ctrl.createOne(body).catch(on_error);
-        created?.push(creation);
+        if (creation != undefined) created?.push(creation);
       }
     }
 
-      return res.send(`\x1b[32mVocê registrou um total de \x1b[0m\x1b[35m${created.length} \x1b[32mno banco de dados\x1b[0m` +
-    '\n\x1b[32mHaviam \x1b[0m\x1b[35m' + req.body.length + '\x1b[0m\x1b[32m de categorias no arquivo.\x1b[0m');
+    return res.send(`\x1b[32mVocê registrou um total de \x1b[0m\x1b[35m${created.length} \x1b[32mprodutos no banco de dados\x1b[0m` +
+      '\n\x1b[32mHaviam \x1b[0m\x1b[35m' + req.body.length + '\x1b[0m\x1b[32m de categorias no arquivo.\x1b[0m');
+  },
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    let bodies: Object[] = req.body;
+    let updateds: Produto[] = [];
+
+    //De nome para id, pronto para a inserção
+    for (let i: number = 0 ; i < bodies.length; i++){
+      let body = bodies[i];
+      const prod = body as any;  
+
+
+      if (typeof (prod.imagens) !== 'undefined') 
+      prod.imagens = (prod.imagens as string).split(',');
+      else prod.imagens = [];
+
+      prod.tipo = uppercase(prod.tipo);
+      prod.subtipo = uppercase(prod.subtipo);
+      prod.marca = uppercase(prod.marca);
+      prod.modelo = uppercase(prod.modelo);
+      prod.versao = uppercase(prod.versao)
+
+      prod.id_tipo = (await ctrl.getCatId('Tipos', prod.tipo));
+      prod.id_subtipo = (await ctrl.getCatId('Subtipo', prod.subtipo));
+      prod.id_marca = (await ctrl.getCatId('Marca', prod.marca));
+      prod.id_modelo = (await ctrl.getCatId('Modelo', prod.modelo));
+      prod.id_versao = (await ctrl.getCatId('Versao', prod.versao));
+      prod.id_prodSku = (await ctrl.getId(prod.prodSku as string));
+
+
+      if (body != null) {
+        const updated = await ctrl.update(body).catch(on_error);
+        if (updated != null && typeof updated !=='undefined') updateds?.push(prod);
+      }
+    }
+
+    return res.send(`\x1b[32mVocê atualizou um total de \x1b[0m\x1b[35m${updateds.length} \x1b[32mno banco de dados\x1b[0m` +
+      '\n\x1b[32mHaviam \x1b[0m\x1b[35m' + req.body.length + '\x1b[0m\x1b[32m de categorias no arquivo.\x1b[0m');
+    
   },
 
   async create_categoria(req: Request, res: Response, next: NextFunction) {
@@ -77,8 +139,10 @@ export default {
         cat.nome = (cat.nome as string).toUpperCase();
       })
 
-      await ctrl.createCategoria(req.params.cat, body).catch(on_error)
-      return res.send(`\x1b[32mVocê registrou um total de \x1b[0m\x1b[35m${body.length} \x1b[32mno banco de dados\x1b[0m` +
+      let created = await ctrl.createCategoria(req.params.cat, body).catch(on_error)
+      if (created == undefined) created = [];
+
+      return res.send(`\x1b[32mVocê registrou um total de \x1b[0m\x1b[35m${(created  as []).length} \x1b[32mno banco de dados\x1b[0m` +
       '\n\x1b[32mHaviam \x1b[0m\x1b[35m' + req.body.length + '\x1b[0m\x1b[32m produtos no arquivo.\x1b[0m');
     }
 
@@ -99,4 +163,5 @@ export default {
   async get_columns(req: Request, res: Response) {
     return res.json(sProd.skeleton.getAttributes())
   }
+
 }
