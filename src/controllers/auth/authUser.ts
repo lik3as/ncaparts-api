548a610517c758f0e7d0623f84f87f1456353d7c
lik3as <daviacosta2006@gmail.com>
2023-08-11 10:07:40 -0300

    two different authorizations

--- src/controllers/auth/authUser.ts
+++ src/controllers/auth/authUser.ts
@@ -4,13 +4,14 @@ import jwt from 'jsonwebtoken';
 export default {
   handleCookieRequest(req: Request, res: Response) {
     res.clearCookie('token');
-    const {name, passwd} = req.body;
+    const {name, passwd, email} = req.body;
 
     if (name == process.env.MASTER_NAME && passwd == process.env.MASTER_SECRET) {
-      const token = jwt.sign({nome: name, senha: passwd}, process.env.AUTH_SECRET!, { expiresIn: "1h" })
-      return res.cookie('token', token, { maxAge: 10000, httpOnly: false, secure: false }).status(200).send('Cookie Enviado'); 
-    }
+      const token = jwt.sign({username: name, passwd: passwd}, process.env.MASTER_AUTH_SECRET!, { expiresIn: "1h" })
+      return res.cookie('token', token, { maxAge: 10000, httpOnly: false, secure: false }).status(200).send('Cookie Mestre Enviado'); 
+    } 
 
-    return res.status(403).send("Login inválido");
+    const token = jwt.sign({username: name, passwd: passwd, email: email}, process.env.COMMON_AUTH_SECRET!, {expiresIn: "7d"}); 
+    return res.cookie('token', token, {maxAge: 10000, httpOnly: true, secure: true}).status(200).send('Cookie Comum Enviado');
   },
 }
--- src/routes/tables/router.merc.ts
+++ src/routes/tables/router.merc.ts
@@ -5,7 +5,7 @@ import util from '../../middleware/util';
 
 const router = Router();
 
-router.post('/tables/Mercadorias', util.verifyJWT, ctrlMerc.create, ctrlMerc.create_many, ctrlMerc.update);
+router.post('/tables/Mercadorias', util.verifyMasterJWT, ctrlMerc.create, ctrlMerc.create_many, ctrlMerc.update);
 router.get('/tables/Mercadorias', ctrlMerc.get_mercs, ctrlMerc.get_mercs_with_sku, ctrlMerc.get_sugestions);
 router.get('/tables/Mercadorias/Columns', ctrlMerc.get_columns);
 router.get('/tables/Mercadorias/records', ctrlMerc.count);
