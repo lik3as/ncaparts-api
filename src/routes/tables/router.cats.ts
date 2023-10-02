e5a2f6e1a3d7867e2cf05029e101af2144ba76cb
lik3as <daviacosta2006@gmail.com>
2023-10-02 18:03:47 -0300

    refactor: Categorias instead of Cats

--- src/routes/tables/router.cats.ts
+++ src/routes/tables/router.cats.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import prodCtrl from "../../controllers/prodCtrl";
 const router = Router();
 
-router.get("/Cats/:cat", prodCtrl.get_categorias);
-router.post("/Cats/:cat", prodCtrl.create_categoria);
+router.get("/Categorias/:cat", prodCtrl.get_categorias);
+router.post("/Categorias/:cat", prodCtrl.create_categoria);
 
 export default router;
