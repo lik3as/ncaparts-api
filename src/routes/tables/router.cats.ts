1269d4a1af7bde550e51170b21bdb4678b873aa1
lik3as <daviacosta2006@gmail.com>
2023-11-03 15:30:38 -0300

    fix: cats methods are all available in this route now

--- src/routes/tables/router.cats.ts
+++ src/routes/tables/router.cats.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import prodCtrl from "../../controllers/prodCtrl";
+import catsCtrl from "../../controllers/catsCtrl";
+import util from "../../middleware/util";
 const router = Router();
 
-router.get("/Categorias/:cat", prodCtrl.get_categorias);
-router.post("/Categorias/:cat", prodCtrl.create_categoria);
+router.get("/Categorias/:cat", catsCtrl.get_categorias);
+router.get("/Categorias/:cat/columns", catsCtrl.get_cat_columns);
+
+router.delete("/Categorias/:cat", util.verifyMasterJWT, catsCtrl.delete_cat); 
+router.post("/Categorias/:cat", util.verifyMasterJWT, catsCtrl.create_categoria);
 
 export default router;
