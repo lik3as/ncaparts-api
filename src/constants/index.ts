c96700ddc9943ba2d4cafa86df0f7739e98296ab
lik3as <daviacosta2006@gmail.com>
2023-11-17 11:14:55 -0300

    feat: CAT types (maybe this feat should be in util dir)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,14 @@
+import { ModelStatic } from "sequelize";
+import { Grupo, Marca, Modelo, Tipo } from "../models";
+
 export const ANSI_RED = "\x1b[31m";
 export const ANSI_GREEN = "\x1b[32m";
 export const ANSI_BLUE = "\x1b[34m";
 export const ANSI_MAGENTA = "\x1b[35m"
 export const ANSI_RESET = "\x1b[0m";
+
+export type CAT = Tipo | Marca | Modelo | Grupo;
+export type CAT_STATIC = ModelStatic<Tipo | Marca | Modelo | Grupo>;
+export type CAT_NAME = "Tipo" | "Grupo" | "Modelo" | "Marca";
+export type CAT_TABLE_NAME = "Tipos" | "Grupos" | "Modelos" | "Marcas";
+export const CAT_TABLE_NAMES = ["Tipos", "Grupos", "Modelos", "Marcas"]
